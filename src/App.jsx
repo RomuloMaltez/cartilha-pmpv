66d7377786d75cd29d327418f2409e75c2f5b3f9
RomuloMaltez <MALTEZROMULO@GMAIL.COM>
2025-06-27 09:34:26 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CartilhaWebInterativa from './components/CartilhaWebInterativa'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <CartilhaWebInterativa />
+    </div>
+  )
+}
+
+export default App
